58aeee8deb144724ebeaa685c3211dc0888e9930
Ajmin Pradhan <md.ajminpradhan@outlook.com>
2020-08-13 17:39:57 +0800

    Updates

---
+++ precache-manifest.23d8cb3809b50a3adba6e8b1e79f290c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0b6e7529325711f7e47e1ed0aba6a3",
+    "url": "/messenger/index.html"
+  },
+  {
+    "revision": "ed6779cad9d1167edde2",
+    "url": "/messenger/static/css/main.3f1ef1d8.chunk.css"
+  },
+  {
+    "revision": "ffa4646fd6f3b0b07f43",
+    "url": "/messenger/static/js/2.ba285afd.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/messenger/static/js/2.ba285afd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed6779cad9d1167edde2",
+    "url": "/messenger/static/js/main.84862c40.chunk.js"
+  },
+  {
+    "revision": "42b02ae745a4878b278e",
+    "url": "/messenger/static/js/runtime-main.68fb1750.js"
+  }
+]);
